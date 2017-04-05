8fa97be896c68d875247cf60d60bf6328d26db5e
Cinepapaya <jfcadillo951@gmail.com>
2017-04-05 12:22:46 -0500

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Cinepapaya on 4/5/17.
+//  Copyright © 2017 FandangoLatam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
